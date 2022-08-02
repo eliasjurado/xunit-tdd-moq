53d8fa1d98e472b72fb46df8b73b0467cb4eefb0
Elias Luis Jurado Santos <elias.jurado@globant.com>
2022-08-02 16:56:10 -0500

    add other tests with string

--- Calculations.Test/NameTest.cs
+++ Calculations.Test/NameTest.cs
@@ -27,5 +27,38 @@ namespace Calculations.Test
             var result = name.MakeFullName("Elias", "Jurado");
             Assert.Contains("elias", result, StringComparison.InvariantCultureIgnoreCase);
         }
+
+        [Fact]
+        public void MakeFullName_StartsWith()
+        {
+            var name = new Name();
+            var result = name.MakeFullName("Elias", "Jurado");
+            Assert.StartsWith("El", result);
+        }
+
+        [Fact]
+        public void MakeFullName_EndsWith()
+        {
+            var name = new Name();
+            var result = name.MakeFullName("Elias", "Jurado");
+            Assert.EndsWith("urado", result);
+        }
+
+        [Fact]
+        public void MakeFullName_StartsWith_and_EndsWith()
+        {
+            var name = new Name();
+            var result = name.MakeFullName("Elias", "Jurado");
+            Assert.StartsWith("El", result);
+            Assert.EndsWith("urado", result);
+        }
+
+        [Fact]
+        public void MakeFullName_MatchRegexPattern()
+        {
+            var name = new Name();
+            var result = name.MakeFullName("Elias", "Jurado");
+            Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", result);
+        }
     }
 }
