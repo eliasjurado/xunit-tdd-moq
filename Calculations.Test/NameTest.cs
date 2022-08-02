cfbb1621f5316e0617b4d1d9af9fb9cb13e21667
Elias Luis Jurado Santos <elias.jurado@globant.com>
2022-08-02 16:42:24 -0500

    add string test

---
+++ Calculations.Test/NameTest.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace Calculations.Test
+{
+    public class NameTest
+    {
+        [Fact]
+        public void MakeFullNameTest()
+        {
+            var name = new Name();
+            var result = name.MakeFullName("Elias", "Jurado");
+            Assert.Equal("Elias Jurado", result);
+        }
+    }
+}
---
+++ Calculations/Name.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculations
+{
+    public class Name
+    {
+        public string MakeFullName(string firstName, string lastName)
+        {
+            return $"{firstName} {lastName}";
+        }
+    }
+}
