37eb1206fc3fca1a507bd752c5b4dde5b6f000ad
Elias Luis Jurado Santos <elias.jurado@globant.com>
2022-08-03 17:08:07 -0500

    add check values

---
+++ Calculations.Test/CustomerTest.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace Calculations.Test
+{
+    public class CustomerTest
+    {
+        [Fact]
+        public void Customer_CheckNameNotEmpty()
+        {
+            Customer customer = new Customer();
+            Assert.NotNull(customer.Name);
+            Assert.False(string.IsNullOrEmpty(customer.Name));
+        }
+        [Fact]
+        public void Customer_CheckAgeForDiscount()
+        {
+            Customer customer = new Customer();
+            Assert.InRange(customer.Age, 24, 40);
+        }
+    }
+}
---
+++ Calculations/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculations
+{
+    public class Customer
+    {
+        public string Name => "Elias";
+        public int Age => 36;
+
+
+    }
+}
