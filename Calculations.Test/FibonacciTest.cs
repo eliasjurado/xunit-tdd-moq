88f51cc6094d13b94a669e949e848088f827fa51
Elias Luis Jurado Santos <elias.jurado@globant.com>
2022-08-03 16:57:25 -0500

    add test number which is or is not part of the array

---
+++ Calculations.Test/FibonacciTest.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+
+namespace Calculations.Test
+{
+    public class FibonacciTest
+    {
+        [Fact]
+        public void Fibonacci_DoesNotIncludeZero()
+        {
+            Fibonacci fibonacci = new Fibonacci();
+            Assert.All(fibonacci.FiboNumbers, n => Assert.NotEqual(0, n));
+        }
+
+        [Fact]
+        public void Fibonacci_Includes13()
+        {
+            Fibonacci fibonacci = new Fibonacci();
+            Assert.Contains(13, fibonacci.FiboNumbers);
+        }
+        [Fact]
+        public void Fibonacci_DoesNotIncludes4()
+        {
+            Fibonacci fibonacci = new Fibonacci();
+            Assert.DoesNotContain(4, fibonacci.FiboNumbers);
+        }
+
+        [Fact]
+        public void Fibonacci_CheckCollection()
+        {
+            var expectedCollection = new List<int>() { 1, 1, 2, 3, 5, 8, 13 };
+            Fibonacci fibonacci = new Fibonacci();
+            Assert.Equal(expectedCollection, fibonacci.FiboNumbers);
+        }
+    }
+}
