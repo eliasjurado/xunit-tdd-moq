a6ba42a40199833e01f55e043b9e47d7bb17ff8b
Elias Luis Jurado Santos <elias.jurado@globant.com>
2022-08-03 13:04:36 -0500

    add we can invoke attributes

--- Calculations/Name.cs
+++ Calculations/Name.cs
@@ -2,6 +2,7 @@
 {
     public class Name
     {
+        public string NickName { get; set; }
         public string MakeFullName(string firstName, string lastName)
         {
             return $"{firstName} {lastName}";
