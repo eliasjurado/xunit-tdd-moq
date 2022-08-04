8c701c3f13977362a0d4ca23cd62f98079cd4360
Elias Luis Jurado Santos <elias.jurado@globant.com>
2022-08-04 07:38:23 -0500

    add grouping using traits

--- Calculations.Test/FibonacciTest.cs
+++ Calculations.Test/FibonacciTest.cs
@@ -4,7 +4,9 @@ namespace Calculations.Test
 {
     public class FibonacciTest
     {
+
         [Fact]
+        [Trait("Category", "Numbers")]
         public void Fibonacci_DoesNotIncludeZero()
         {
             Fibonacci fibonacci = new Fibonacci();
@@ -12,18 +14,22 @@ namespace Calculations.Test
         }
 
         [Fact]
+        [Trait("Category", "Numbers")]
         public void Fibonacci_Includes13()
         {
             Fibonacci fibonacci = new Fibonacci();
             Assert.Contains(13, fibonacci.FiboNumbers);
         }
+
         [Fact]
+        [Trait("Category", "Numbers")]
         public void Fibonacci_DoesNotIncludes4()
         {
             Fibonacci fibonacci = new Fibonacci();
             Assert.DoesNotContain(4, fibonacci.FiboNumbers);
         }
 
+        [Trait("Category", "Collections")]
         [Fact]
         public void Fibonacci_CheckCollection()
         {
