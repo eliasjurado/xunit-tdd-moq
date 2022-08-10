054fb8dbd0b29febeb4383b1b4c42e06d6d3e726
Elias Luis Jurado Santos <elias.jurado@globant.com>
2022-08-10 09:47:22 -0500

    add sprout method for legacy code, in order to create a test

---
+++ LegacyTesting/SproutMethod/LegacyClass.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace SproutMethod
+{
+    public class LegacyClass
+    {
+        public void AppendDictionary<TKey, TValue>(Dictionary<TKey, TValue> fromDict, Dictionary<TKey, TValue> toDict)
+        {
+            Dictionary<TKey, TValue> validItems = GetValidItems(fromDict, toDict);
+
+            foreach (var item in validItems)
+            {
+                //We can solve the repeated key problem with an if statement
+                //if (!toDict.ContainsKey(item.Key)) { toDict.Add(item.Key, item.Value); }
+                //But it will be going to be imposible to test. Thats why we use GetValidItems,
+                //in order to respect the open/close principle
+
+                toDict.Add(item.Key, item.Value);
+            }
+        }
+
+        //This is the sprout method,which is going to help us to solve the problem when AppendDictionary contains repeated keys
+        public Dictionary<TKey, TValue> GetValidItems<TKey, TValue>(Dictionary<TKey, TValue> fromDict, Dictionary<TKey,
+            TValue> toDict)
+        {
+            var result = new Dictionary<TKey, TValue>();
+
+            foreach (var item in fromDict)
+            {
+                if (!toDict.ContainsKey(item.Key)) //here we implement the solution not in the legacy code
+                    result.Add(item.Key, item.Value);
+            }
+            return result;
+        }
+    }
+}
