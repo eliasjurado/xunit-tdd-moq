feb1bd0538fb04ab722ffff28b7e8cc4db3e14d7
Elias Luis Jurado Santos <elias.jurado@globant.com>
2022-08-10 14:43:28 -0500

    add creation of the method and its interface

---
+++ 04-TDDSample/TDDSample/DefaultPasswordValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace TDDSample
+{
+    public class DefaultPasswordValidator : IPasswordValidator
+    {
+        public bool Validate(string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
