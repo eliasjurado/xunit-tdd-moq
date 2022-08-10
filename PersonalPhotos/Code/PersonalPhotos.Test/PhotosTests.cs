b4af8bdfa8e07d0390d11fe8cb31a1627dcd05bf
Elias Luis Jurado Santos <elias.jurado@globant.com>
2022-08-10 08:37:34 -0500

    add mock to test controller method

--- PersonalPhotos/Code/PersonalPhotos.Test/PhotosTests.cs
+++ PersonalPhotos/Code/PersonalPhotos.Test/PhotosTests.cs
@@ -4,8 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 using Moq;
 using PersonalPhotos.Controllers;
 using PersonalPhotos.Models;
-using System;
-using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -19,23 +17,26 @@ namespace PersonalPhotos.Test
         {
             // Arrange
             var session = Mock.Of<ISession>();
-            session.Set("User", Encoding.UTF8.GetBytes("a@b.com"));
-            var context = Mock.Of<HttpContext>(x=> x.Session == session);
+            session.Set("User", Encoding.UTF8.GetBytes("elias.jurado@globant.com"));
+            var context = Mock.Of<HttpContext>(x => x.Session == session);
             var accessor = Mock.Of<IHttpContextAccessor>(x => x.HttpContext == context);
 
+            //It's needed to add Mocks of dependecies where are injected
             var fileStorage = Mock.Of<IFileStorage>();
             var keyGen = Mock.Of<IKeyGenerator>();
             var photoMetadata = Mock.Of<IPhotoMetaData>();
 
+            //Also we need a mock for the file we're going to upload
             var fromFile = Mock.Of<IFormFile>();
             var model = Mock.Of<PhotoUploadViewModel>(x => x.File == fromFile);
 
+            //finally, we can test our controller initializing it
             var controller = new PhotosController(keyGen, accessor, photoMetadata, fileStorage);
 
-            //Act
+            //Then call the action we're going to test
             var result = await controller.Upload(model) as RedirectToActionResult;
 
-            //Assert
+            //Assert. And what's going to happen
             Assert.Equal("Display", result.ActionName, ignoreCase: true);
         }
 
