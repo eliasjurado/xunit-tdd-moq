aa1e5416232840d20e2fc532eb15f5ddb45e8565
Elias Luis Jurado Santos <elias.jurado@globant.com>
2022-08-10 14:43:59 -0500

    add tests with values and refactoring it to make it robuster

---
+++ 04-TDDSample/TDDSampleTest/PasswordTest.cs
@@ -0,0 +1,40 @@
+using TDDSample;
+
+namespace TDDSampleTest
+{
+    public class PasswordTest
+    {
+        [Fact]
+        public void Validate_GivenLongerThan8Chars_ReturnsTrue()
+        {
+            var targetTest = new DefaultPasswordValidator();
+            var password = "1234567890";
+            var validationResult = targetTest.Validate(password);
+
+            Assert.True(validationResult);
+        }
+
+        [Fact]
+        public void Validate_GivenShorterThan8Chars_ReturnsFalse()
+        {
+            var targetTest = new DefaultPasswordValidator();
+            //We can set values manually...
+            //var password = "1234567";
+            //... but it would be robuster if we create a method
+            var password = MakeString(7);
+            var validationResult = targetTest.Validate(password);
+
+            Assert.False(validationResult);
+        }
+
+        private string MakeString(int lenght)
+        {
+            string result = "";
+            for (int i = 0; i < lenght; i++)
+            {
+                result += "a";
+            }
+            return result;
+        }
+    }
+}
